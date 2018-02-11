5fcefb742faa9607b64b9a516b96b88089ea2d68
gverni <g.verni@gmail.com>
2018-02-11 20:20:51 +0000

    Moved all the logic to VityalityMain component

--- App.js
+++ App.js
@@ -1,53 +1,15 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, Text, View } from 'react-native';
-import { Vitality } from './Vitality-react.js';
+import { AppRegistry } from 'react-native';
+import Vivacity from './src/VivacityMain'
 
+global.vivacity = new Vivacity()
 
-export default class Movies extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-    };
-  }
-
-  componentDidMount() {
-    var vitality = new Vitality();
-    return vitality
-      .login({username: '', password: ''})
-      .then(() => {
-        console.log('Getting weekly point');
-        return vitality.getWeeklypoints();
-      })
-      .then(weeklyPoints => {
-        this.setState(
-          {
-            isLoading: false,
-            text: 'Weekly points ' + weeklyPoints,
-          },
-          function() {
-            // do something with new state
-          }
-        );
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
-
+export default class VivacityApp extends Component {
   render() {
-    if (this.state.isLoading) {
-      return (
-        <View style={{ flex: 1, paddingTop: 20 }}>
-          <ActivityIndicator />
-        </View>
-      );
-    }
-
     return (
-      <View style={{ flex: 1, paddingTop: 20 }}>
-        <Text>{this.state.text}</Text>
-      </View>
+     <Vivacity />
     );
   }
 }
+
+AppRegistry.registerComponent('VivacityApp', () => VivacityApp);
